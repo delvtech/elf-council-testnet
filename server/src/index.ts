f0f7f3001dce0f031697ec01ed3f32fd9ee40ecd
Matthew Brown <mattfdbrown@gmail.com>
2021-10-12 09:07:46 -0700

    add packages, make files for merkle server

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+
+/**
+ * Required External Modules
+ */
+
+/**
+ * App Variables
+ */
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Server Activation
+ */
