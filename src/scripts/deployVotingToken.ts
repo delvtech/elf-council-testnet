b4469cf122e6f091cd20246aac4ecf8cad4a8702
Danny Delott <dannydelott@gmail.com>
2021-10-11 17:19:17 -0700

    seed balances of element token

--- src/scripts/deployVotingToken.ts
+++ src/scripts/deployVotingToken.ts
@@ -1,4 +1,6 @@
+import { parseUnits } from "@ethersproject/units";
 import { Signer } from "ethers";
+import { signers } from "src/addresses/signers";
 import { MockERC20__factory } from "types/factories/MockERC20__factory";
 import { MockERC20 } from "types/MockERC20";
 
@@ -7,6 +9,12 @@ export async function deployVotingToken(signer: Signer): Promise<MockERC20> {
   const tokenContract = await tokenDeployer.deploy(
     "Element Governance Token",
     "ELFI"
+  );
+  await Promise.all(
+    signers.map((walletAddress) =>
+      // seed balances
+      tokenContract.setBalance(walletAddress, parseUnits("50", 18))
+    )
   );
 
   return tokenContract;
